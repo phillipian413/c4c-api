c23359e0f592df8f96751c3dc6a8f718f6c0f757
christian ouangbara <christian.ouangbara@gmail.com>
2024-12-26 17:01:25 +0100

    OK

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class AbstractModel(models.Model):
+
+    uuid = models.UUIDField(
+        _("UUID"),
+        default=uuid.uuid4(),
+        unique=True,
+        editable=False,
+        db_index=True
+    )
+    created_at = models.DateTimeField(_("Created At") ,auto_now=False, auto_now_add=False)
+    updated_at = models.DateTimeField(_("Updated At") ,auto_now=False, auto_now_add=False)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
